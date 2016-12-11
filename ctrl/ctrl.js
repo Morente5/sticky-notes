7b29ffb1c4f4b61aed4ab2767cc94c4247ea632b
Alejandro Morente <morente5@hotmail.es>
2016-12-11 09:53:28 +0100

    Some changes to save or load data from notes

--- ctrl/ctrl.js
+++ ctrl/ctrl.js
@@ -4,9 +4,10 @@ class MainCtrl {
 		this.view = new View();
 
 		this.noteCtrls = [];
+		this.storageCtrl = new StorageCtrl(this, this.model);
 
 		this.bindEvents();
-		this.storageCtrl.load();
+		this.storageCtrl.materialize();
 
 	}
 
@@ -28,20 +29,26 @@ class StickyNoteCtrl {
 		this.ctrl = mainCtrl;
 		this.stickyNote = this.ctrl.model.addStickyNote(title, content, date);
 		this.viewNote = new ViewNote(this.ctrl.view.container, title, content, date);
+		this.ctrl.storageCtrl.save(this.stickyNote.position);
 		this.bindEvents();
 	}
 
 	bindEvents() {
 		this.viewNote.deleteButton.addEventListener('click', () => {
 			this.delete();
+			var position = this.stickyNote.position;
+			var len = this.ctrl.model.stickyNotes.length;
+			this.ctrl.storageCtrl.save(position, len);
 		});
 
 		this.viewNote.titleField.addEventListener('keyup', () => {
 			this.stickyNote.title = this.viewNote.title;
+			this.ctrl.storageCtrl.save(this.stickyNote.position);
 		});
 
 		this.viewNote.contentField.addEventListener('keyup', () => {
 			this.stickyNote.content = this.viewNote.content;
+			this.ctrl.storageCtrl.save(this.stickyNote.position);
 		});
 
 	}
@@ -51,6 +58,7 @@ class StickyNoteCtrl {
 		this.ctrl.model.stickyNotes.splice(i, 1);
 		this.ctrl.noteCtrls.splice(i, 1);
 		this.viewNote.parent.removeChild(this.viewNote.note);
+		this.ctrl.storageCtrl.delete(this.stickyNote.position);
 	}
 
 }
